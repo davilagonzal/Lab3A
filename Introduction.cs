c1f5b1131e368af5949028bb0d9e856ab49a8bb1
davilagonzal <89435675+davilagonzal@users.noreply.github.com>
2022-02-13 23:05:45 -0500

    Add files via upload

---
+++ Introduction.cs
@@ -0,0 +1,23 @@
+﻿{
+string firstName;
+string lastName;
+int age;
+
+    Console.WriteLine("What's your first name?");
+ firstName = Console.ReadLine();
+
+System.Console.WriteLine("What's your last name?");
+lastName = Console.ReadLine();
+
+string fullName = firstName + " " + lastName;
+
+string firstNameInitial = firstName.Substring(0, 1);
+
+string lastNameInitial = lastName.Substring(0, 1);
+
+System.Console.WriteLine("What's your age?");
+age = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Hello, " + fullName + " your initials are " + firstNameInitial + "." + lastNameInitial + ". " + "and you are " + age + " years old"); 
+
+}
